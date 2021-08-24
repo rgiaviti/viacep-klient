dd05398dfe708927b7ff5e203fee8e189a4526d2
Ricardo Giaviti <ricardo.giaviti@luizalabs.com>
2021-08-24 09:23:42 -0300

    updated gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.21"
     kotlin("plugin.serialization") version "1.5.21"
+    id("maven-publish")
 }
 
 group = "io.github.rgiaviti"
@@ -24,4 +25,48 @@ tasks.test {
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "11"
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/rgiaviti/viacep-klient")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["kotlin"])
+        }
+        publications {
+            create<MavenPublication>("mavenKotlin") {
+                pom {
+                    name.set("ViaCEP Klient")
+                    description.set("A client SDK to retrieve brazilian CEP informations from ViaCEP service")
+                    url.set("https://github.com/rgiaviti/viacep-klient")
+                    licenses {
+                        license {
+                            name.set("MIT License")
+                            url.set("https://opensource.org/licenses/MIT")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("rg")
+                            name.set("Ricardo Giaviti")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:git://github.com:rgiaviti/viacep-klient.git")
+                        developerConnection.set("scm:git:ssh://git@github.com:rgiaviti/viacep-klient.git")
+                        url.set("https://github.com/rgiaviti/viacep-klient")
+                    }
+                }
+            }
+        }
+    }
 }
